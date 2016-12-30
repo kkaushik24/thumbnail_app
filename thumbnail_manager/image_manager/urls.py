8cd1c9c22f368f4c278fb389d5a04e088258c4ca
Ubuntu <ubuntu@kaushik-test-1.c.api-project-617329713936.internal>
2016-12-30 09:41:04 +0000

    thumbnail_app: add image processing logic

---
+++ thumbnail_manager/image_manager/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import url
+from image_manager.views import create_thumbnail
+urlpatterns = [
+url(r'^create-thumbnail/$', create_thumbnail),]
---
+++ thumbnail_manager/image_manager/views.py
@@ -0,0 +1,57 @@
+from PIL import Image
+import urllib2
+import io
+
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+
+# Create your views here.
+
+def create_thumbnail(request):
+	"""
+	This view creates the thumbnail for the imageurl given in get
+	query.
+	"""
+	# get query for image processing
+	image_url = request.GET.get('image_url')
+	height = request.GET.get('height')
+	width = request.GET.get('width')
+	error_list_str = []
+	# response dict for the image
+	response_dict = {'image_url': image_url,
+					  'height': height,
+					  'width': width}
+	
+	# sanity check for correct get params
+	if image_url is None:
+		error_msg = "No image url in get parameter"
+		error_list_str.append(error_msg)
+	if height is None:
+		error_msg = "No height is provided"
+		error_list_str.append(error_msg)
+	if width is None:
+		error_list_str.append(error_msg)
+
+	size = width, height
+	# if there is no error try to process the image url
+	if not error_list_str:
+		try:
+			image_data = urllib2.urlopen(image_url)
+			image_file = io.BytesIO(image_data.read())
+			img = Image.open(image_file)
+		except:
+			error_msg = "Image could not be loaded"
+			error_list_str.append(error_msg)
+
+		try:
+			img.thumbnail(size)
+		except:
+			error_msg = "Thumbnail could not be created"
+			error_list_str.append(error_msg)
+	if error_list_str:
+		# if there is error 
+		# send the json response for the error
+		response_dict.update({"errors": error_list_str})
+		return JsonResponse(response_dict)
+	else:
+		return HttpResponseRedirect("/thanks")
